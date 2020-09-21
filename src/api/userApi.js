8cf062e5b5b161b50b8f157e007e9a880098c6b9
Daniel Rooein <Daniel.Rooein@gmail.com>
2020-09-20 20:13:25 -0400

    Add http call mocking, json-server, json schema fake, etc.

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,26 @@
 import 'whatwg-fetch'; // This takes care of the browsers that dont' support fetch natively
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(userId) {
+  return del(`users/${userId}`);
+}
+
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url){
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
